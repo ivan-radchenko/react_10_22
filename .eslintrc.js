cb4212a38600778db9ab92ef340875a75a69ad87
ivan-radchenko <ivan.msk95@gmail.com>
2022-11-16 21:27:12 +0300

    homework_4_done

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    "prettier",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:storybook/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier", "react-hooks", "jest"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: false,
+      },
+    ],
+    "react/prop-types": 0,
+    "linebreak-style": ["error", "unix"],
+    quotes: ["warn", "double"],
+    semi: ["warn", "always"],
+    "react/react-in-jsx-scope": "off",
+    "react/display-name": "off",
+  },
+  overrides: [
+    {
+      files: ["webpack.config.js"],
+      rules: {
+        "@typescript-eslint/no-var-requires": ["off"],
+      },
+    },
+  ],
+};
